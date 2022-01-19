702a25c8c4d479e08e67041fef7e7c545144a6ca
Catherine_AMF <catherineamf@gmail.com>
2022-01-19 19:52:27 +0200

    Change let on const, removed from function

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 'use strict';
 
 document.getElementById('add-to-list-button').addEventListener('click', onAddToListClick);
+const toDoList = document.getElementById('list');
+const titleInput = document.getElementById('list-element-title');
 
 function onAddToListClick(event) {
     event.preventDefault();
 
-    let titleInput = document.getElementById('list-element-title');
     let elementTitle = titleInput.value.trim();
     if (elementTitle.length !== 0) {
         let li = document.createElement('li');
         li.innerText = elementTitle;
-
-        let toDoList = document.getElementById('list');
         toDoList.appendChild(li);
 
         titleInput.value = '';
